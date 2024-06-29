8b8d37cea64f0151676f4b2b8d9e0c6a6289f5e3
XuTiany1 <tiany12138@gmail.com>
2024-06-29 10:54:52 -0400

    created all three interfaces that builds upon the most fundamental interface of layer.py

---
+++ layers/hidden_layer.py
@@ -0,0 +1,30 @@
+from abc import ABC
+from numpy import outer
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from interfaces.layer import NetworkLayer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
