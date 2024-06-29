6d8394f4baf456e9e4cd8c333d7a489294727efb
XuTiany1 <tiany12138@gmail.com>
2024-06-29 17:23:44 -0400

    update experiment constants

--- utils/experiment_constants.py
+++ utils/experiment_constants.py
@@ -20,6 +20,10 @@ class LateralInhibitions(Enum):
     RELU_INHIBITION = 'RELU'
     SOFTMAX_INHIBITION = 'SOFTMAX'
     HOPFIELD_INHIBITION = 'HOPFIELD'
+    WTA_INHIBITION = 'WTA'
+    GAUSSIAN_INHIBITION = 'GAUSSIAN'
+    NORM_INHIBITION = 'NORM'
+
 
 class FunctionTypes(Enum):
     """
@@ -53,5 +57,32 @@ class ExperimentTypes(Enum):
     
     
 class Purposes(Enum):
+    """
+    ENUM CLASS
+    Defines purposes for loggin/file creation purposes
+    @instance attr.
+    """
     TRAIN_ACCURACY = 'TRAIN'
     TEST_ACCURACY = 'TEST'
+    
+
+class DataSetNames(Enum):
+    """
+    ENUM CLASS
+    Defines name of dataset used
+    @instance attr.
+    """
+    MNIST = 'MNIST'
+    E_MNIST = 'E_MNIST'
+    FASHING_MNIST = ' FASHION_MNIST'
+    
+    
+class ExperimentPhases(Enum):
+    """
+    ENUM CLASS
+    Defines name of dataset used
+    @instance attr.
+    """
+    BASE = 'BASE'
+    RECONSTRUCTION = 'RECONSTRUCTION'
+    FREEZING_WEIGHTS = 'FREEZING_WEIGHTS'
